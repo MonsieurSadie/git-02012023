b11ba66a2baeb9aee78328d69a1faa9beec7440b
MonsieurSadie <kevin.lesur@gmail.com>
2022-12-30 11:27:16 +0100

    prepared drift mechanic

--- Assets/Scripts/Avatar.cs
+++ Assets/Scripts/Avatar.cs
@@ -17,7 +17,18 @@ public class Avatar : MonoBehaviour
 		Vector3 inputDirection = new Vector3(Input.GetAxis("Horzontal"), 0, Input.GetAxis("Vertical"));
 
 		Vector3 desiredDir = inputDirection.normalized;
+		float desiredAngleChange = Vector3.Angle(desiredDir,  currentDir);
+		if(desiredAngleChange > 90)
+		{
+			// turn on drift mechanic
+		}
 		currentDir = Vector3.RotateTowards(currentDir, desiredDir, maxDirRot * Time.deltaTime, 0);
+
+		if(currentDir.magnitude > 1)
+		{
+			Debug.LogWarning("Shouldn't have a non normalized direction");
+		}
+
 		Vector3 deltaMove = pow * currentDir * Time.deltaTime;
 	}
 }
